850ef881993e07a2b2db8799ed704ac19a936dc3
Barbi Borrello <biborrello@hotmail.com>
2024-05-03 18:02:05 -0300

    .

---
+++ instalando-typescript/index.ts
@@ -0,0 +1,12 @@
+
+
+function mostrarNombre(nombre: string) {
+  console.log("Tu nombre es", nombre);
+}
+
+mostrarNombre("Barbara");
+
+/**
+ * ! llamando en la terminal:
+ * ?  ts-node index.ts
+ */
