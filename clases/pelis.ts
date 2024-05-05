1b683cd1278a12b126ba0fc4bca9b33e73d74b8d
Barbi Borrello <biborrello@hotmail.com>
2024-05-05 17:51:49 -0300

    .

---
+++ clases/pelis.ts
@@ -0,0 +1,27 @@
+class Peli {
+  title: string;
+  rating: number;
+
+  getPeli(){
+    return this;
+  }
+}
+
+function main (){
+// instancias
+const peli = new Peli();
+peli.title = "Una peli";
+peli.rating = 5;
+
+const peli2 = new Peli();
+peli2.title = "Otra peli";
+peli2.rating = 1;
+
+console.log(peli);
+console.log(peli2);
+
+
+
+}
+
+main()
